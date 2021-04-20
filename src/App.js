6839d5bce3b327a7a54f1692639b3aae62fb6089
patrick hrabos <phrabos@gmail.com>
2021-04-20 15:52:26 -0400

    wip card flip logic, status: all cards flip at same time

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ export default class App extends Component {
   state = {
     cards: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8],
     cardsFlipped: 0,
+    cardStatus: 'hidden',
+  }
+
+  handleCardClick = () => {
+    if(this.state.cardStatus === 'hidden'){
+      this.setState({cardStatus: 'show' })
+    }
+    if(this.state.cardStatus === 'show'){
+      this.setState({cardStatus: 'hidden' })
+    }
+    
   }
 
   render() {
@@ -17,9 +28,14 @@ export default class App extends Component {
         </h1><br/>
       <div className="App">
 
-        {this.state.cards.map((card) => {
-         return <p className="card">
-           <span className="hidden">{card}</span></p>
+        {this.state.cards.map((card, i) => {
+         return <p 
+         key={card + i}
+         onClick={this.handleCardClick}
+         className='card'
+
+         >
+           <span className={this.state.cardStatus}>{card}</span></p>
         })
         }
       </div>
